d599b39a170f8e8f01940dfb7ed2e95a80864bd0
Vlad Galcev <Vladgalcev03@gmail.com>
2022-05-01 17:29:25 +0300

    module5lesson7

--- game02.js
+++ game02.js
@@ -1,42 +1,83 @@
 'use strict'
 
-const game = () => {
-  const firstNumber = +prompt('Первое число от', '10');
-  const secondNumber = +prompt('Второе число до', '20');
-  const arrToGuess = [];
-  const NumberSelected =  Math.floor(Math.random() * (secondNumber - firstNumber + 1)) + firstNumber;
-  console.log(NumberSelected);
-  let attemptsCount = (secondNumber - firstNumber) * 0.3;
-  while(attemptsCount > 0) {
-    let i = 0;
-    i < arrToGuess.length;
-    i += 1;
-    const numberToGuess = +prompt(`Введите число от ${firstNumber} и до ${secondNumber}`);
-    arrToGuess.push(numberToGuess);
-    console.log(arrToGuess);
-    if (!numberToGuess) {
-      break;
-    }
-    if (isNaN(numberToGuess)) {
-      alert('Введи число!'); 
-    } else if (arrToGuess[i] === numberToGuess) {
-      alert('Это число вы уже вводили');
-    } else if (numberToGuess < NumberSelected) {
-      attemptsCount -= 1;
-      alert('Больше!');
-    } else if (numberToGuess > NumberSelected) {
-      attemptsCount -= 1;
-      alert('Меньше!');
-    } else if (numberToGuess === NumberSelected) {
-      alert('Правильно!');
-      break;
-    } 
-    if (attemptsCount <= 0) {
-      alert('Вы истратили все попытки');
-      break;
-    }
-    alert(`У вас осталось попыток: ${attemptsCount}`);
-  }
-  alert( 'Игра завершена.' );
+const arrToGuess = [];
+const firstNumber = +prompt('первое число', '10');
+const secondNumber = +prompt('второе число', '20');
+const NumberSelected =  Math.floor(Math.random() * (secondNumber - firstNumber + 1)) + firstNumber;
+console.log(`Загаданное число ${NumberSelected}`);
+let attemptsCount = (secondNumber - firstNumber) * 0.3;
+let attempts = 0;
+let correctGuess = false;
+
+const getUserGuess = () => {
+  const userGuess = +prompt(`Введите число между ${firstNumber} и ${secondNumber}`);
+  return userGuess;
 }
-game()
+
+const checkGuessInRange = (guess) => {
+  if (guess < firstNumber || guess > secondNumber) {
+    alert(`Ваше число ${guess} не входит в диапазон от ${firstNumber} до ${secondNumber}`);
+    return false;
+  }
+  return true;
+}
+
+const checkUserGuess = (guess) => {
+  for (let i = 0; i < arrToGuess.length; i++) {
+    if (arrToGuess[i] === guess) {
+      alert(`Вы уже вводили это число ${guess}`);
+      return false;
+    }
+  }
+  return true;
+}
+
+const addToArrToGuess = (guess) => {
+  arrToGuess.push(guess);
+}
+
+const updateGuessAttempts = () => {
+  attempts++;
+}
+
+const displayResults = () => {
+  if (correctGuess) {
+    alert(`Поздравляем! Вы угадали число ${NumberSelected} за ${attempts} попыток`);
+  } else {
+    alert(`Вы не угадали число ${NumberSelected} за ${attempts} попытки`);
+  }
+}
+
+const compareGuess = (guess) => {
+  if (guess === NumberSelected) {
+    correctGuess = true;
+  }
+}
+
+const notifyUser = (userGuess, NumberSelected) => {
+  if (userGuess > NumberSelected) {
+    alert(`Меньше!`);
+  } else if (userGuess < NumberSelected) {
+    alert(`Больше!`);
+  }
+}
+
+while (attempts <= attemptsCount && !correctGuess) {
+  const userGuess = getUserGuess();
+  if (!userGuess) {
+    alert ('Игра закончена');
+    break;
+  };
+  if (checkGuessInRange(userGuess)) {
+    if (checkUserGuess(userGuess)) {
+      notifyUser(userGuess, NumberSelected);
+      addToArrToGuess(userGuess);
+      compareGuess(userGuess);
+      updateGuessAttempts();
+      console.log(arrToGuess);
+    }
+  }
+  if (attempts === attemptsCount) {
+    displayResults();
+  }
+}
